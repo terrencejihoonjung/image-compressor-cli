b61deae19af12982f0a367153a915e799c2b956d
terrencejihoonjung <terrencejung928@gmail.com>
2023-08-18 18:37:31 -0700

    imported modules

---
+++ compress-images.js
@@ -0,0 +1,7 @@
+#!/usr/bin/env node
+
+// import required modules
+const fs = require("node:fs");
+const path = require("node:path");
+const sharp = require("sharp");
+const { program } = require("command");
